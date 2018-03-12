e951c4cf44317cd1275b7c65ae4b7800dc132035
Shanelle Savoy <mznellienellz@gmail.com>
2018-03-12 18:03:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Shanelle"
-var height = "74"
+var height = "60"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+6)
+console.log(height)
